82c44139cd196fe125674160a33fa38d9f1be9ad
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2025-06-10 16:25:06 -0500

    Updated prepare script.

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -3,7 +3,7 @@ import os
 """
 Prepare a copy of the iMessage database and address book database
 """
-os.system("cp ~/Library/Messages/chat.db dbt/dbs")
+os.system("cp ~/Library/Messages/chat.db a_data_home/dbs/chat.db")
 os.system(
-    """cp ~/Library/Application\\ Support/AddressBook/AddressBook-v22.abcddb dbt/dbs"""
+    """cp ~/Library/Application\\ Support/AddressBook/AddressBook-v22.abcddb a_data_home/dbs/AddressBook-v22.abcddb"""
 )
